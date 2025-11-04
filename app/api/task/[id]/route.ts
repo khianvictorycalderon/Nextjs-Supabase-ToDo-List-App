ae973bcfca5d0ab20bfbcd0c0427ed44e8f9ba9d
khian calderon <ckhianvictory@gmail.com>
2025-11-04 17:24:03 +0800

    Updated update patch

--- app/api/task/[id]/route.ts
+++ app/api/task/[id]/route.ts
@@ -1,12 +1,22 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 // For marking if the task is done or pending
 // By default, a task is marked pending when created
-export async function PATCH(req: NextRequest) {
+export async function PATCH(
+    req: NextRequest, 
+    {params}: {params: Promise<{id: string}>} 
+) {
+    const { id: taskId } = await params;
+    const changeType = await req.json();
+    
+    console.log(`Task with ID ${taskId} successfully marked as ${changeType.status}`)
 
+    return NextResponse.json({
+        message: "Successfully mocked updated!"
+    }, { status: 200 });
 }
 
 // For deleting a task
-export async function DELETE(req: NextRequest) {
+// export async function DELETE(req: NextRequest) {
 
-}
+// }
