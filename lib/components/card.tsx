adc3c2869139d1ba3455bc310b565b446ac74839
khian calderon <ckhianvictory@gmail.com>
2025-11-04 15:47:26 +0800

    Updated finished task background

--- lib/components/card.tsx
+++ lib/components/card.tsx
@@ -2,7 +2,7 @@
 
 export const PendingCard = ({taskName}: CardProps) => (
     <div className="bg-gray-600 p-4 rounded-md shadow">
-        <p className="text-xl">{taskName}</p>
+        <p className="text-xl font-semibold tracking-wider">{taskName}</p>
         <div className="flex w-full mt-4 gap-4">
             <button className="flex-1 bg-blue-600 py-2 rounded-lg cursor-pointer transition duration-300 hover:bg-blue-500">Mark as Done</button>
             <button className="flex-1 bg-red-600 py-2 rounded-lg cursor-pointer transition duration-300 hover:bg-red-500">Delete</button>
@@ -11,8 +11,8 @@ export const PendingCard = ({taskName}: CardProps) => (
 );
 
 export const FinishedCard = ({taskName}: CardProps) => (
-    <div className="bg-gray-600 p-4 rounded-md shadow">
-        <p className="text-xl">{taskName}</p>
+    <div className="bg-gray-800 p-4 rounded-md shadow">
+        <p className="text-xl font-semibold tracking-wider">{taskName}</p>
         <div className="flex w-full mt-4 gap-4">
             <button className="flex-1 bg-green-600 py-2 rounded-lg cursor-pointer transition duration-300 hover:bg-green-500">Mark as Pending</button>
             <button className="flex-1 bg-red-600 py-2 rounded-lg cursor-pointer transition duration-300 hover:bg-red-500">Delete</button>
