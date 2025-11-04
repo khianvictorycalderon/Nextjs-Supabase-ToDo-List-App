af7699de5a730b61a3f37d767a1ee93a123a265d
khian calderon <ckhianvictory@gmail.com>
2025-11-04 17:14:06 +0800

    Completed APIs + created route.ts

---
+++ app/api/task/[id]/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest } from "next/server";
+
+// For marking if the task is done or pending
+// By default, a task is marked pending when created
+export async function PATCH(req: NextRequest) {
+
+}
+
+// For deleting a task
+export async function DELETE(req: NextRequest) {
+
+}
--- lib/components/card.tsx
+++ lib/components/card.tsx
@@ -1,12 +1,23 @@
 'use client';
 
-const handleMarkAsDone = async (taskId: string) => {
-}
+import axios from "axios";
 
-const handleMarkAsPending = async (taskId: string) => {
+const handleUpdateStatus = async (taskId: string, status: "done" | "pending") => {
+    try {
+        await axios.patch(`/api/task/${taskId}`, { status });
+        console.log(`Task with ID ${taskId}, successfully marked as ${status}!`);
+    } catch (err: any) {
+        alert(`Unable to marked task with ID ${taskId} to ${status}: ${err.message}`);
+    }
 }
 
 const handleDeleteTask = async (taskId: string) => {
+    try {
+        await axios.delete(`/api/task/${taskId}`);
+        console.log(`Task with ID ${taskId}, successfully deleted!`);
+    } catch (err: any) {
+        alert(`Unable to delete task with ID ${taskId}: ${err.message}`);
+    }
 }
 
 export const PendingCard = ({taskId, taskName}: CardProps) => {
@@ -14,7 +25,7 @@ export const PendingCard = ({taskId, taskName}: CardProps) => {
         <div className="bg-gray-600 p-4 rounded-md shadow">
             <p className="text-xl font-semibold tracking-wider">{taskName}</p>
             <div className="flex w-full mt-4 gap-4">
-                <button onClick={() => handleMarkAsDone(taskId)} className="flex-1 bg-blue-600 py-2 rounded-lg cursor-pointer transition duration-300 hover:bg-blue-500">Mark as Done</button>
+                <button onClick={() => handleUpdateStatus(taskId, "done")} className="flex-1 bg-blue-600 py-2 rounded-lg cursor-pointer transition duration-300 hover:bg-blue-500">Mark as Done</button>
                 <button onClick={() => handleDeleteTask(taskId)} className="flex-1 bg-red-600 py-2 rounded-lg cursor-pointer transition duration-300 hover:bg-red-500">Delete</button>
             </div>
         </div>
@@ -26,7 +37,7 @@ export const FinishedCard = ({taskId, taskName}: CardProps) => {
         <div className="bg-gray-800 p-4 rounded-md shadow">
             <p className="text-xl font-semibold tracking-wider">{taskName}</p>
             <div className="flex w-full mt-4 gap-4">
-                <button onClick={() => handleMarkAsPending(taskId)} className="flex-1 bg-green-600 py-2 rounded-lg cursor-pointer transition duration-300 hover:bg-green-500">Mark as Pending</button>
+                <button onClick={() => handleUpdateStatus(taskId, "pending")} className="flex-1 bg-green-600 py-2 rounded-lg cursor-pointer transition duration-300 hover:bg-green-500">Mark as Pending</button>
                 <button onClick={() => handleDeleteTask(taskId)} className="flex-1 bg-red-600 py-2 rounded-lg cursor-pointer transition duration-300 hover:bg-red-500">Delete</button>
             </div>
         </div>
