78335ee896fe2303ee513fdeb01c481f21ec9425
khian calderon <ckhianvictory@gmail.com>
2025-11-04 12:23:19 +0800

    Added blank div

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,8 @@
 
 export default function App() {
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-neutral-900 text-white">
-      <div className="text-center">
-        <h1 className="text-2xl font-bold">Next.js + Tailwind</h1>
-        <h1 className="text-2xl italic">To Do List App</h1>
-      </div>
+    <div className="bg-amber-400 text-white p-4">
+      
     </div>
   )
 }
