cf33b9957c31327187f518956381079657b81a0f
khian calderon <ckhianvictory@gmail.com>
2025-11-04 12:26:45 +0800

    Added title and description

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@
 
 export default function App() {
   return (
-    <div className="bg-amber-400 text-white p-4">
-      
+    <div className="bg-amber-400 p-4">
+      <h1 className="text-4xl font-bold text-center pt-4">ToDo List App</h1>
+      <h2 className="text-2xl italic text-center pb-4">(Next.js + Supabase)</h2>
     </div>
   )
 }
