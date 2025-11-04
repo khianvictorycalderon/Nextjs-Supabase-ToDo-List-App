337b77524ae7f387298d956f5127420b5d7371d0
khian calderon <ckhianvictory@gmail.com>
2025-11-04 18:24:10 +0800

    Added type safety + prevent default

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,8 @@ export default function App() {
     axios.get("api/task").then(res => setTasks(res.data.tasks));
   },[]);
 
-  const handleAddTask = (taskName: string) => {
-
+  const handleAddTask = (e: React.FormEvent<HTMLFormElement>, taskName: string) => {
+    e.preventDefault();
   }
 
   return (
@@ -26,7 +26,7 @@ export default function App() {
       <div className="bg-gray-900 text-white p-4">
         <h1 className="text-4xl font-bold text-center pt-4">ToDo List App</h1>
         <h2 className="text-2xl italic text-center pb-4">(Next.js + Supabase)</h2>
-        <form onSubmit={() => handleAddTask(taskInputName)} className="my-4 justify-center items-center flex flex-col lg:flex-row gap-2 lg:gap-6">
+        <form onSubmit={(e) => handleAddTask(e, taskInputName)} className="my-4 justify-center items-center flex flex-col lg:flex-row gap-2 lg:gap-6">
           <label htmlFor="task-name-input" className="text-lg font-semibold">Task:</label>
           <input onChange={(e) => setTaskInputName(e.target.value)} name="task-name-input" className="bg-gray-50 text-lg p-1 rounded-md w-full lg:w-[30%] outline-0 focus:ring-blue-600 focus:ring-2 shadow focus:shadow-2xl text-black" type="text" />
           <input className="font-bold py-2 px-6 cursor-pointer bg-green-600 text-white rounded-md w-full lg:w-auto hover:bg-green-500 transition duration-300" type="submit" value="Add Task"/>
